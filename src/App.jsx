3d1ab31a1594788c277d253299f6babe887ecf49
imas-1 <samispiridon15@gmail.com>
2026-07-21 11:41:54 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import { DataProvider } from './context/DataContext';
 import { SnackbarProvider } from './context/SnackbarContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { SecurityProvider, useSecurity } from './context/SecurityContext';
 import PrivateRoute from './components/PrivateRoute';
 import BottomNav from './components/BottomNav';
 import UpdatePrompt from './components/UpdatePrompt';
+import LockScreen from './components/LockScreen';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Stats from './pages/Stats';
@@ -31,29 +34,43 @@ function Shell({ children }) {
   );
 }
 
+/** Afișează ecranul de blocare (PIN/Face ID) doar dacă userul e logat ȘI are un PIN setat ȘI e blocat momentan. */
+function LockGate({ children }) {
+  const { user } = useAuth();
+  const { hasPin, locked } = useSecurity();
+  if (user && hasPin && locked) return <LockScreen />;
+  return children;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <DataProvider>
-          <SnackbarProvider>
-            <UpdatePrompt />
-            <Shell>
-              <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-                <Route path="/statistici" element={<PrivateRoute><Stats /></PrivateRoute>} />
-                <Route path="/calendar" element={<PrivateRoute><Calendar /></PrivateRoute>} />
-                <Route path="/spatii" element={<PrivateRoute><Spaces /></PrivateRoute>} />
-                <Route path="/spatii/:spaceId" element={<PrivateRoute><SpaceDetail /></PrivateRoute>} />
-                <Route path="/obiective" element={<PrivateRoute><Goals /></PrivateRoute>} />
-                <Route path="/obiective/:goalId" element={<PrivateRoute><GoalDetail /></PrivateRoute>} />
-                <Route path="/setari" element={<PrivateRoute><Settings /></PrivateRoute>} />
-              </Routes>
-            </Shell>
-          </SnackbarProvider>
-        </DataProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <DataProvider>
+            <SecurityProvider>
+              <SnackbarProvider>
+                <UpdatePrompt />
+                <LockGate>
+                  <Shell>
+                    <Routes>
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                      <Route path="/statistici" element={<PrivateRoute><Stats /></PrivateRoute>} />
+                      <Route path="/calendar" element={<PrivateRoute><Calendar /></PrivateRoute>} />
+                      <Route path="/spatii" element={<PrivateRoute><Spaces /></PrivateRoute>} />
+                      <Route path="/spatii/:spaceId" element={<PrivateRoute><SpaceDetail /></PrivateRoute>} />
+                      <Route path="/obiective" element={<PrivateRoute><Goals /></PrivateRoute>} />
+                      <Route path="/obiective/:goalId" element={<PrivateRoute><GoalDetail /></PrivateRoute>} />
+                      <Route path="/setari" element={<PrivateRoute><Settings /></PrivateRoute>} />
+                    </Routes>
+                  </Shell>
+                </LockGate>
+              </SnackbarProvider>
+            </SecurityProvider>
+          </DataProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
